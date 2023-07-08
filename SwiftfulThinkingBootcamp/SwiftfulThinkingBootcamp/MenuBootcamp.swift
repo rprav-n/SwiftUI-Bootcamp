d5a9ca5da8d728867a56ce24df94eba4ecbbc77b
Praveen <rpraveen.engineer@gmail.com>
2023-07-08 19:09:30 +0530

    Added MenuBootcamp

---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcamp/MenuBootcamp.swift
@@ -0,0 +1,28 @@
+//
+//  MenuBootcamp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Praveen R on 08/07/23.
+//
+
+import SwiftUI
+
+struct MenuBootcamp: View {
+    var body: some View {
+        Menu("Click me") {
+            Button("One") {
+                
+            }
+            Button("Two") {
+                
+            }
+        }
+        
+    }
+}
+
+struct MenuBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBootcamp()
+    }
+}
